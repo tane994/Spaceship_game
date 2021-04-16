eeba2dfa8d3ddb950f2355dfb793acb9a07b9130
andres <andres.tanesini94@gmail.com>
2021-04-16 09:39:27 +0200

    creating a settings for the game, a ship that is going to be controlled by the player and wrote methods for movement of such ship.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -8,21 +10,46 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
+
+        self.ship = Ship(self)
 
 
     def run_game(self):
         "Start the main loop for the game"
         while True:
-        #watch for keyboard event and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            #Make the most recently drawn screen visible.
-            pygame.display.flip()
+
+    def _check_events(self):
+        # watch for keyboard event and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        # Update images on the screen, and flip to a new screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all setting for the game"""
+
+    def __init__(self):
+        """Initializing settings."""
+        #Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
